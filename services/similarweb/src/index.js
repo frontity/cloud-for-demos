a8c6f4c68b0d9b6dd0e54969d64afa4182e9dfc4
Luis Herranz <luisherranz@gmail.com>
2019-01-29 15:01:14 +0100

    chore(similarweb): remove now-env, we'll send the api key via POST

--- services/similarweb/src/index.js
+++ services/similarweb/src/index.js
@@ -1,4 +1,3 @@
-require('now-env');
 const fetch = require('cross-fetch');
 const { parse } = require('url');
 const urlencodedBodyParser = require('urlencoded-body-parser');
@@ -8,7 +7,6 @@ const cors = require('micro-cors')();
 module.exports = cors(async (req, res) => {
   try {
     const incomingData = await urlencodedBodyParser(req);
-    console.log(incomingData);
     const { host } = parse(incomingData.url);
     const endpoints = [
       'visits',
@@ -16,7 +14,7 @@ module.exports = cors(async (req, res) => {
       'average-visit-duration',
       'bounce-rate',
     ];
-    const response = await fetch(`https://api.similarweb.com/v1/website/${host}/total-traffic-and-engagement/${endpoints[0]}?api_key=${process.env.API_KEY}&start_date=2018-11&end_date=2018-11&main_domain_only=false&granularity=monthly`)
+    const response = await fetch(`https://api.similarweb.com/v1/website/${host}/total-traffic-and-engagement/${endpoints[0]}?api_key=${incomingData.apiKey}&start_date=2018-11&end_date=2018-11&main_domain_only=false&granularity=monthly`)
     const body = await response.json();
     return send(res, 200, body);
   } catch (error) {
